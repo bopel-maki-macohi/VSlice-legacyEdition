63918368914558233e8c94a5bedccdca80f44072
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-04-03 09:46:13 -0500

    tabs being werid and idk how to fix lmao

--- source/ChartingState.hx
+++ source/ChartingState.hx
@@ -634,10 +634,10 @@ class ChartingState extends MusicBeatState
 				else
 					UI_box.selected_tab += 1;
 
+				if (UI_box.selected_tab > UI_box.numTabs - 1)
+					UI_box.selected_tab = 0;
 				if (UI_box.selected_tab < 0)
 					UI_box.selected_tab = UI_box.numTabs - 1;
-				if (UI_box.selected_tab >= UI_box.numTabs)
-					UI_box.selected_tab = 0;
 			}
 
 			if (FlxG.keys.justPressed.RIGHT || FlxG.keys.justPressed.D)
